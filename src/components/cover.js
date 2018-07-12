ad73957f7da94665467ce422f4b60d1e805ca562
Aravind Iyer <aravindiyer@gmail.com>
2018-07-12 19:02:08 +0530

    Add Noticebox component. Add credits and call-to-action to Cover component.

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,16 +1,62 @@
+import React from "react"
 import styled from "styled-components"
 import logo from "../../images/logo.svg"
 
 const Cover = styled.div`
+  position: relative;
   margin: 3rem auto 2rem;
-  width: 60vw;
+  width: 65vw;
   max-width: 800px;
-  height: 60vh;
+  height: 65vh;
   border-radius: 1rem;
   background-color: #859e6b;
   background-image: url(${logo});
-  background-size: 55vmin;
+  background-size: 45vmin;
   background-repeat: no-repeat;
   background-position: center;
+  padding: 1rem;
+  h2 {
+    color: #542938;
+    font-weight: 400;
+    padding: 0;
+    margin: 0;
+  }
 `
-export default Cover
+
+const Credits = styled.div`
+`
+
+const Caption = styled.div`
+  text-align: right;
+  position: absolute;
+  bottom: 1rem;
+  right: 1rem;
+`
+
+const Cta = styled.a`
+  display: inline-block;
+  border-radius: 0.3rem;
+  padding: 0.3rem 0;
+  margin: 0;
+  width: 9rem;
+  background: #542938;
+  text-align: center;
+  text-decoration: none;
+  color: white;
+  border: 0;
+  font-size: 1.1em;
+  font-weight: "700";
+`
+
+export default () => (
+  <Cover>
+    <Credits>
+      <h2>Maarga Sangeet</h2>
+      <h2>by S. Balachander</h2>
+    </Credits>
+    <Caption>
+      <h2>Experience the music</h2>
+      <Cta href="intro.html">Listen Now</Cta>
+    </Caption>
+  </Cover>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Container from "../components/container"
-import Blockquote from "../components/blockquote"
+import Noticebox from "../components/noticebox"
 import Pagebackground from "../components/pagebackground"
 import Button from "../components/button"
 import Masthead from "../components/masthead"
@@ -12,7 +12,6 @@ const indexPage = () => (
     <Masthead />
     <Cover />
     <Container>
-      <h1>Chandraveena</h1>
       <div>
         <h2>What is Maarga Sangeet?</h2>
         <p>
@@ -24,22 +23,14 @@ const indexPage = () => (
         <p>
           Chandraveena is a modern take on the historical instrument, the <em>been</em>. It enables the artist to ...
         </p>
-        <div>
+        <Noticebox>
           <p>
             Get started by listening to a few clips and an introduction by S. Balachander.
             <br/>
             <br/>
             <Button href="intro.html">Listen Now</Button>
           </p>
-        </div>
-      </div>
-      <Blockquote>
-        Music is a spiritual journey. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, alias.
-      </Blockquote>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis reprehenderit, eos consequatur, tenetur soluta enim accusantium laborum suscipit nostrum error provident asperiores aliquam sequi quas, omnis doloremque accusamus quisquam similique deserunt sint corporis, id ab. Aperiam dignissimos, totam sequi laborum quia eveniet commodi dolore, quam adipisci quisquam quaerat, aut veniam aliquid corrupti possimus illo tenetur tempora ratione laudantium ea doloremque quidem? Qui provident fuga amet laudantium dolor iure, enim sed alias minus quos architecto, consequuntur nulla veniam saepe! Repellat maiores dolorum cupiditate perspiciatis tempore, deserunt iure sed amet ipsa libero consequatur porro facilis nesciunt veritatis debitis molestiae saepe consectetur animi.
-        </p>
+        </Noticebox>
       </div>
     </Container>
   </div>
