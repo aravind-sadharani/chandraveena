fce675e16c80c28f865ce25e490f40a642a3e443
Aravind Iyer <aravindiyer@gmail.com>
2018-07-12 12:48:25 +0530

    Apply Chandraveena font theme to Typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,52 @@
 import Typography from "typography"
 
-const typography = new Typography()
+const chandraveenaTheme = {
+  baseFontSize: '18px',
+  baseLineHeight: 1.5,
+  scaleRatio: 2.5,
+  headerFontFamily: [
+    "Gentium Book Basic",
+    "serif",
+  ],
+  bodyFontFamily: [
+    "Noto Serif",
+    "serif",
+  ],
+  googleFonts: [
+    {
+      name: 'Sansita',
+      styles: [
+        '400',
+        '700',
+      ],
+    },
+    {
+      name: 'Gentium Book Basic',
+      styles: [
+        '400',
+        '700',
+      ],
+    },
+    {
+      name: 'Noto Serif',
+      styles: [
+        '400',
+      ]
+    },
+  ],
+  overrideStyles: () => ({
+    h1: {
+      fontFamily: ['Sansita'].join(','),
+      fontWeight: '700',
+    },
+    blockquote: {
+      fontFamily: ['Sansita'].join(','),
+      fontWeight: '400',
+      fontSize: '1.4em',
+    },
+  })
+}
+
+const typography = new Typography(chandraveenaTheme)
 
 export default typography
