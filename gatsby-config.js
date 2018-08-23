f7ad7acba90e9da184e71be96b088dff79f2a634
Aravind Iyer <aravindiyer@gmail.com>
2018-08-23 18:21:23 +0530

    Update PWA to "standalone"

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#e1dbdd`,
         theme_color: `#542938`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `images/Favicon.png`,
       },
     },
