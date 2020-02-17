a75d3ed2c57e51f7a5f7c3b182021337c0682426
Aravind Iyer <aravind@Aravinds-MacBook-Pro.local>
2020-02-17 14:30:29 +0530

    Update localsearch

--- src/components/localsearch.js
+++ src/components/localsearch.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react'
 import Helmet from "react-helmet"
+import {Container, Outer} from "./containers"
+import Masthead from "./masthead"
+import Footer from "./footer"
+import styled from "styled-components"
 
-// Search component
+const Searchwrapper = styled.div`
+  table {
+    margin-bottom: 0;
+  }
+  button {
+    padding: 6px 27px 0px 27px;
+    border-radius: 3px;
+  }
+  .gsc-input-box {
+    border-radius: 0;
+    padding: 0;
+  }
+  .gsib_b {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+`
+
 class LocalSearch extends Component {
   render() {
     return (
-      <>
+      <Outer>
         <Helmet>
           <script async src="https://cse.google.com/cse.js?cx=014117222396928509438:tvkphh9meod"></script>
-        </Helmet>
-        <div className="gcse-search"></div>
-      </>
+          </Helmet>
+        <Masthead />
+        <Container>
+          <Searchwrapper>
+            <div className="gcse-search"></div>
+          </Searchwrapper>
+        </Container>
+        <Footer search/>
+      </Outer>
     )
   }
 }
